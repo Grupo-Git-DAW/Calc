35ccb08b7ead185eb52dba68166360caf99c007d
Galera80 <59236419+Galera80@users.noreply.github.com>
2019-12-26 03:04:14 +0100

    Add files via upload
    Version3

--- main.c
+++ main.c
@@ -63,29 +63,89 @@ void sumar()
 int main() 
 {
     char n;
+    int x=1;
     
     printf("¿Qué quieres hacer?\n");
-    //CREAR MENÚ 
+   
+    printf("S o s para Sumar\n");
+    printf("R o r para Restar\n");
+    printf("M o m para Multiplicar\n");
+    printf("D o d para Dividir\n");
+    printf("T o t para Trigonometria\n");
+    printf("E o e  para Exponenciales\n");
+    printf("R o r para Raices\n");
+    printf("L o l para Logaritmo\n");
+    printf("X o x para Salir\n");
+    
+    
     scanf("%c",&n);
+           
+    while(x != 0)
+    {    
+        switch
+           {
             
-    switch(n)
-    {
-        case 's':
-        case 'S': sumar();
-        break;
+                case 's':
+                case 'S': sumar();
+                x=0;
+                    break;
         
-        case 'r':
-        case 'R': restar();
-        break;
+                case 'r':
+                case 'R': restar();
+                x=0;
+                    break;
         
-        case 'd':
-        case 'D': dividir();
-        break;
+                case 'd':
+                case 'D': dividir();
+                x=0;
+                    break;
         
-        case 'm':
-        case 'M': multiplicar();
-        break;
+                case 'm':
+                case 'M': multiplicar();
+                x=0;
+                    break;
+     /*   
+                case 't':
+                case 'T': trigonometria();
+      *         x=0;
+                    break;
+        
+                case 'e':
+                case 'E': exponencial();
+      *         x=0;
+                    break;
+        
+                case 'r':
+                case 'R': raiz();
+      *         x=0;
+                    break;
+        
+                case 'l':
+                case 'L': logatirmo();
+      *         x=0;
+                    break;
+        
+                case 'x':
+                case 'X': salir();
+      *         x=0;
+                    break;
+       */ 
+                default:
+                printf("Caracter no valido");
+                x=0;
+                
+                    
+             }
+                
+        
+            return 0; 
+                  
     }
-    return 0;
+                       
+  
+ 
+        
+    
+    
+        
 }
-
